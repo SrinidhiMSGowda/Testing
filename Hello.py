6326d4fac6325e5164d028e4d7a27403812a2a4c
SrinidhiMSGowda <nidhisri56@gmail.com>
2025-08-08 17:45:17 +0530

    commited

--- Hello.py
+++ Hello.py
@@ -49,3 +49,9 @@ print(text[2:5]) #Slicing with step positive
 print()
 print(text[-4:-1]) #Negative slicing 
 
+# ...existing code...
+
+# Print numbers from 1 to 10
+for i in range(1, 11):
+    print(i)
+# ...existing code...
